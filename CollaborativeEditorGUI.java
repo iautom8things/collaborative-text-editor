df7b241c091f797625975f06ddd44850c243397d
unknown <Andre@.(none)>
2011-10-24 10:39:33 -0500

    Skeletal GUI

---
+++ CollaborativeEditorGUI.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+
+public class CollaborativeEditorGUI{
+  
+   public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+   
+   /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Collaborative Text Editor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the menubar
+        JMenuBar menuBar = createMenuBar();
+        frame.setJMenuBar(menuBar);
+                
+        //Add the text area
+        JTextArea textArea = new JTextArea();
+        frame.getContentPane().add(textArea);
+
+        //Display the window.
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+    
+    //Creates and returns a JMenuBar with the appropriate JMenu items
+    private static JMenuBar createMenuBar(){
+      JMenuBar result = new JMenuBar();
+      JMenu fileMenu = new JMenu("File");
+      fileMenu.add("Open");
+      fileMenu.add("Save");
+      fileMenu.add("Save As...");
+      fileMenu.add("Collaborate");
+      fileMenu.add("Exit");
+      result.add(fileMenu);
+      return result;
+    }
+  
+}
