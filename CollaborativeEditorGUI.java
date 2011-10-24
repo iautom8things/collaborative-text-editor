d65eaedf322dd17cde97bb799473ecaf1437389d
ahnguyen <andre.harrison3@gmail.com>
2011-10-24 11:37:25 -0500

    Revised comments

--- CollaborativeEditorGUI.java
+++ CollaborativeEditorGUI.java
@@ -3,8 +3,7 @@ import javax.swing.*;
 public class CollaborativeEditorGUI{
   
    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
+        //Create and show this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
