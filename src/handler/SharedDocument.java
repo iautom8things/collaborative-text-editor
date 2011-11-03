22bd2feb896d92fa800c06a0cbae87abe73cc549
Manuel Zubieta <mazubieta@gmail.com>
2011-11-02 23:35:40 -0500

    implemented insert/delete text of the SharedDocument.java

--- src/handler/SharedDocument.java
+++ src/handler/SharedDocument.java
@@ -1,6 +1,5 @@
 package handler;
-import user.User;
-import user.CTEUserManager;
+import user.*;
 
 
 /**
@@ -40,15 +39,49 @@ public class SharedDocument implements Document {
      */
     public synchronized int getLength ( ) { return _buffer.length(); }
 
-    public synchronized void insertText ( String writer, String str ) throws OutOfBoundsException {
+    /*
+     *  Given a writer and a string, insert the string at the writer's
+     *  position then update the writer's position to be at the end of the
+     *  inserted text, while also updating any other writer that was initially
+     *  beyond the position of the writer.
+     */
+    public synchronized void insertText ( String writer, String str ) throws OutOfBoundsException , UserNotFoundException {
+        User usr = _userManager.getUser(writer);
+        TextPosition pos = usr.getPosition();
+        int index = pos.getPosition();
+        int len = str.length();
 
+        _buffer.insert(index, str);
+        _userManager.updateBeyond(pos, len);
+        pos.incrementBy(len);
     }
 
-    public synchronized void deleteText ( User writer, TextPosition toPos ) {
+    /*
+     *  Given a writer and a new position, delete all character between the
+     *  two positions and update all TextPositions between these two positions
+     *  so they all end up at the front anchor position while decrementing all
+     *  postions beyond the back position by the size of the chunk of text
+     *  removed.
+     */
+    public synchronized void deleteText ( String writer, TextPosition toPos ) throws OutOfBoundsException, UserNotFoundException {
+        User usr = _userManager.getUser(writer);
+        TextPosition fromPos = usr.getPosition();
 
-    }
+        if (fromPos.isBeyond(toPos)) {
+            // If fromPos is actually beyond toPos, then swap them
+            TextPosition temp = toPos;
+            toPos = fromPos;
+            fromPos = temp;
+        }
 
-    public synchronized void moveCursorTo ( User writer, TextPosition toPos ) throws OutOfBoundsException {
+        int fromIndex = fromPos.getPosition();
+        int toIndex = toPos.getPosition();
+        int range = fromIndex - toIndex;
+
+        _buffer.delete(fromIndex, toIndex);;
+        _userManager.updateBetween(fromPos, toPos);
+        _userManager.updateBeyond(toPos, range);
+        _userManager.setCursorForUser(writer, fromPos);
 
     }
 
