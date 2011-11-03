e372a50289e1a75361a42ec1f9cc2a0c0006314f
Manuel Zubieta <mazubieta@gmail.com>
2011-11-03 15:19:18 -0500

    altered the gui so the open button in the menu bar now works. it reads in a file directly to the JTextPane. Next step is to read it into the document instead

--- src/gui/EditorGUI.java
+++ src/gui/EditorGUI.java
@@ -2,12 +2,23 @@ package gui;
 
 import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.channels.FileChannel;
+import java.nio.MappedByteBuffer;
+import java.nio.charset.Charset;
 
 public class EditorGUI{
-  
+
+    JTextPane textPane;
+    JScrollPane scrollPane;
+    JFrame frame;
   public EditorGUI(){
   }
-  
+
   public void launch(){
     javax.swing.SwingUtilities.invokeLater(new Runnable() {
       public void run() {
@@ -15,31 +26,32 @@ public class EditorGUI{
       }
     });
   }
-  
+
   private void createAndShow(){
-    JFrame frame = new JFrame("Collaborative Text Editor");
+    frame = new JFrame("Collaborative Text Editor");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
+
     //Add the menubar
     frame.setJMenuBar(createMenuBar());
-    
+
     //Add the text area
-    JTextPane textPane = new JTextPane();
-    JScrollPane scrollPane = new JScrollPane(textPane);
+    textPane = new JTextPane();
+    scrollPane = new JScrollPane(textPane);
+    // textPane.setEditable(false); // This might end up being set so we can manually keep track of what is displayed
     textPane.addKeyListener(new KeystrokeListener());
     frame.getContentPane().add(scrollPane);
-    
+
     //Display the window.
     frame.pack();
     frame.setSize(800, 600);
     frame.setVisible(true);
   }
-  
+
   //Creates and returns a JMenuBar with the appropriate JMenu items
   private JMenuBar createMenuBar(){
     JMenuBar result = new JMenuBar();
     JMenu fileMenu = new JMenu("File");
-    
+
     //Create menu items
     JMenuItem open = new JMenuItem("Open");
     JMenuItem save = new JMenuItem("Save");
@@ -47,7 +59,7 @@ public class EditorGUI{
     JMenuItem collab = new JMenuItem("Collaborate");
     JMenuItem exitCollab = new JMenuItem("Exit Collaboration");
     JMenuItem exitApp = new JMenuItem("Exit Application");
-    
+
     //Add menu items to JMenu
     fileMenu.add(open);
     fileMenu.add(save);
@@ -55,47 +67,66 @@ public class EditorGUI{
     fileMenu.add(collab);
     fileMenu.add(exitCollab);
     fileMenu.add(exitApp);
-    
+
     //Register menu items with listeners
     open.addActionListener(new OpenListener());
     exitApp.addActionListener(new ExitListener());
-    
+
     result.add(fileMenu);
     return result;
   }
-  
+
   /*************************************
    * Listeners for specific JMenuItems *
    *************************************/
-  
-  //Display the dialog to open a file
-  private class OpenListener implements ActionListener{
-    public void actionPerformed(ActionEvent e){
-      System.out.println("Display the open file dialog.");
+
+    //Display the dialog to open a file
+    private class OpenListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            System.out.println("Display the open file dialog.");
+            //Setup FileChooser
+            JFileChooser chooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter( "TXT & DAT Only", "txt", "dat" );
+            chooser.setFileFilter(filter);
+            int returnVal = chooser.showOpenDialog(frame);
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                File f = chooser.getSelectedFile();
+                try {
+                    FileInputStream stream = new FileInputStream(f);
+                    FileChannel fChannel = stream.getChannel();
+                    MappedByteBuffer byteBuff = fChannel.map(FileChannel.MapMode.READ_ONLY, 0, fChannel.size());
+
+                    String contents = Charset.defaultCharset().decode(byteBuff).toString();
+                    textPane.setText(contents);
+                    stream.close();
+                }
+                catch (IOException ioe) { System.out.println(ioe.getMessage()); }
+            }
+
+        }
     }
-  }
-  
+
   //Exit the application
   private class ExitListener implements ActionListener{
     public void actionPerformed(ActionEvent e){
       System.exit(0);
     }
   }
-  
+
   /**************************************/
-  
+
   private class KeystrokeListener implements KeyListener{
     public void keyPressed(KeyEvent e){
       //System.out.println("Key pressed.");
     }
-    
+
     public void keyReleased(KeyEvent e){
       //System.out.println("Key released.");
     }
-    
+
     public void keyTyped(KeyEvent e){
       System.out.println("Key typed: " + e.getKeyChar() + "(" + e.getKeyCode() + ")");
     }
   }//End KeystrokeListener
-  
+
 }//End CollaborativeEditorGUI
