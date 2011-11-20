d6738a5f0cc36eb2454aaf378a576ff93b289ed5
Manuel Zubieta <mazubieta@gmail.com>
2011-11-20 16:54:12 -0600

    omg it like totally works

--- src/server/ClientTest.java
+++ src/server/ClientTest.java
@@ -3,14 +3,25 @@ package server;
 import java.lang.SecurityManager;
 import java.lang.Exception;
 import java.rmi.*;
+import user.*;
+import java.net.InetAddress;
+import java.rmi.ConnectException;
+import java.awt.Color;
 
 public class ClientTest {
 
     public static void main ( String[] args ) throws Exception{
-      if (System.getSecurityManager() == null) {
-          System.setSecurityManager(new ZeroSecurityManager());
-          System.out.println(System.getSecurityManager());
-      }
-        ServerCommandListener temp = (ServerCommandListener) Naming.lookup("rmi://localhost/CommandListener");
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new ZeroSecurityManager());
+            System.out.println("Success setting Security Manager.");
+        }
+        try {
+            ServerCommandListenerInterface temp = (ServerCommandListenerInterface) Naming.lookup("rmi://localhost/CommandListener");
+            CTEUser u = new CTEUser("name", InetAddress.getLocalHost(), Color.RED);
+            temp.registerClient(u);
+        }
+        catch (ConnectException ce) { System.out.println("Unable to connect to server!"); }
+        catch (Exception e) { e.printStackTrace(); }
+        System.out.println("Success receiving ServerCommandListener.");
     }
 }
