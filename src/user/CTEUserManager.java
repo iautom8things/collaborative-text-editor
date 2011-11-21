1c056916824c8995b6fc109f34647d77347d21cb
Manuel Zubieta <mazubieta@gmail.com>
2011-11-21 09:37:44 -0600

    Updated comments for CTEUserManager

--- src/user/CTEUserManager.java
+++ src/user/CTEUserManager.java
@@ -19,54 +19,44 @@ public class CTEUserManager implements Serializable {
      */
     public CTEUserManager ( ) { _users = new ConcurrentHashMap<String, CTEUser>(); }
 
-    /**
-     * Add a new user with the specified userID and IPAddress to the container
-     * @Requires
-     *      userID != null
-     *      userID is unique - not contained in _users already
-     *      IPAddress != null
-     * @Ensures
-     *      user will be added
-     */
-    public synchronized void addUser ( CTEUser user ) throws InvalidUserIDException {
-        if (_users.containsValue(user)) { throw new InvalidUserIDException(user.getUserID()); }
-        _users.put(user.getUserID(), user);
-    }
+
+    /***********
+     * Queries *
+     **********/
+
 
     /**
-     * Remove the user with the specified userID from the collection
-     * @Requires
-     *      the user is contained in this CTEUserManager
-     * @Ensures
-     *      the user is not conatined in this CTEUserManager
-     */
-    public synchronized void removeUser ( String userID ) throws UserNotFoundException {
-        if (_users.containsKey(userID)) { _users.remove(userID); }
-        else { throw new UserNotFoundException(userID); }
-    }
-
-    /**
-     * Set the position of the user with the userID to the given cursorPosition
-     * @Requires
-     *      CTEUser with userID is contained in this CTEUserManager
-     * @Ensures
-     *      the value of the cursor position for this user is the same as cursorPosition
-     */
-    public synchronized void setCursorForUser ( String userID, TextPosition cursorPosition ) throws UserNotFoundException, OutOfBoundsException {
-        CTEUser currentUser = getUser(userID);
-        currentUser.setPosition(cursorPosition);
-    }
-
-    /**
-     * Returns the number of CTEUsers contained in this CTEUserManager
+     * Returns the number of CTEUsers contained in this CTEUserManager.
      */
     public synchronized int getNumberOfUsers ( ) { return _users.size(); }
 
-    public synchronized boolean contains ( String name ) { return _users.containsKey(name); }
+    /**
+     * Checks if the given CTEUser is contained in the CTEUserManager.
+     *
+     * Requires:
+     *      CTEUser != null
+     */
+    public synchronized boolean contains ( CTEUser user ) { return _users.containsValue(user); }
 
     /**
-     * Returns the CTEUser with the specified userID
-     * @Requires
+     * Return a String representation of the CTEUserManager.
+     */
+    public String toString() { return "CTEUserManager{ " + "_users=" + _users + " }"; }
+
+    /**
+     * Return an Iterable Collection of the CTEUsers this CTEUserManager
+     * manages.
+     */
+    public synchronized Collection<CTEUser> getUsers ( ) { return _users.values(); }
+
+    /**
+     * Returns the CTEUser with the specified userID.
+     *
+     * Note:
+     *      This might be deprecated since CTEUsers are never solely
+     *      represented by _JUST_ a String name.
+     *
+     * Requires:
      *      a CTEUser with the specified userID is contained in this CTEUserManager
      */
     public synchronized CTEUser getUser ( String userID ) throws UserNotFoundException {
@@ -76,10 +66,59 @@ public class CTEUserManager implements Serializable {
         return result;
     }
 
+
+    /************
+     * Commands *
+     ***********/
+
+
+    /**
+     * Add a new user with the specified userID and IPAddress to the
+     * container.
+     *
+     * Requires:
+     *      userID != null
+     *      userID is unique - not contained in _users already
+     *      IPAddress != null
+     * Ensures:
+     *      user will be added
+     */
+    public synchronized void addUser ( CTEUser user ) throws InvalidUserIDException {
+        if (_users.containsValue(user)) { throw new InvalidUserIDException(user.getUserID()); }
+        _users.put(user.getUserID(), user);
+    }
+
+    /**
+     * Remove the user with the specified userID from the collection.
+     *
+     * Requires:
+     *      the user is contained in this CTEUserManager
+     * Ensures:
+     *      the user is not conatined in this CTEUserManager
+     */
+    public synchronized void removeUser ( String userID ) throws UserNotFoundException {
+        if (_users.containsKey(userID)) { _users.remove(userID); }
+        else { throw new UserNotFoundException(userID); }
+    }
+
+    /**
+     * Set the position of the user with the userID to the given cursorPosition.
+     *
+     * Requires:
+     *      CTEUser with userID is contained in this CTEUserManager
+     * Ensures:
+     *      the value of the cursor position for this user is the same as cursorPosition
+     */
+    public synchronized void setCursorForUser ( String userID, TextPosition cursorPosition ) throws UserNotFoundException, OutOfBoundsException {
+        CTEUser currentUser = getUser(userID);
+        currentUser.setPosition(cursorPosition);
+    }
+
     /**
      * Given a pivot point, every TextPosition of a User that is beyond the
      * pivot will be incremented (if amount > 0) or decremented (if amount < 0)
      * by the amount specified.
+     *
      * @Requires
      *      pivot != null
      *      amount != null
@@ -119,7 +158,4 @@ public class CTEUserManager implements Serializable {
         }
     }
 
-    public String toString() { return "CTEUserManager{ " + "_users=" + _users + " }"; }
-
-    public synchronized Collection<CTEUser> getUsers ( ) { return _users.values(); }
 }
